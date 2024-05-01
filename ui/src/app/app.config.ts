3abdb2f9c0952b14553007031cdb209d0560ebfe
Matheus Nascimento <matheus.conexa@gmail.com>
2024-04-30 23:13:01 -0300

    feat: Adding material UI and RetryInterceptor

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,8 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { RetryInterceptor } from './interceptors/RetryInterceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule), provideAnimationsAsync(), provideAnimationsAsync(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RetryInterceptor,
+      multi: true,
+    }
+  ],
 };
--- ui/src/app/pages/auth/login/login.component.ts
+++ ui/src/app/pages/auth/login/login.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Login } from '../../../interface/login';
 import { Router } from '@angular/router';
-
+import { HttpClient, HttpContext, HttpContextToken, HttpHeaders } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
+export const ERROR_COUNT = new HttpContextToken(() => 0);
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -15,14 +18,42 @@ export class LoginComponent {
     username: '',
     password: ''
   };
+  authenticated = false;
+  loading = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: HttpClient, private _snackBar: MatSnackBar) { }
 
   onLogin() {
-    if (this.loginObject.username === 'admin' && this.loginObject.password === 'admin') {
-      this.router.navigateByUrl('/home');
-    } else {
-      alert('Credenciais inválidas!')
-    }
+    this.authenticate()
+  }
+
+  authenticate() {
+
+    const headers = new HttpHeaders({
+      authorization: 'Basic ' + btoa(this.loginObject.username + ':' + this.loginObject.password),
+      'Content-Type': 'application/json',
+
+    });
+    this.loading = true;
+    this.http.post<{ token: string }>(`api/auth`, null, {
+      headers: headers, withCredentials: false
+      
+    }).subscribe(response => {
+      if (response.token) {
+        this.authenticated = true;
+        this.router.navigateByUrl('/home');
+      } else {
+        this.authenticated = false;
+        this._snackBar.open('Credencias inválidas', 'Close', {
+          duration: 2000,
+        });
+      }
+      return true;
+    })
+      .add(() => {
+        this.loading = false;
+        console.log('Finalizou');
+      });
+
   }
 }
