1f56e4c5cb4e14cf7a23a0b3212aad2daaff1776
Matheus Nascimento <matheus.conexa@gmail.com>
2024-05-09 21:08:58 -0300

    fix: Usage of record to AddressForm and UserForm

--- src/main/java/ufba/br/api/dto/AddressForm.java
+++ src/main/java/ufba/br/api/dto/AddressForm.java
@@ -4,67 +4,20 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints. Pattern;
 import jakarta.validation.constraints.PositiveOrZero;
 
-public class AddressForm {
+public record AddressForm(
     @NotEmpty(message = "Rua não pode ser vazio")
-    private String street;
+    String street,
     @NotEmpty(message = "Cidade não pode ser vazio")
-    private String city;
+    String city,
     @NotEmpty(message = "Estado não pode ser vazio")
-    private String state;
+    String state,
     @Pattern(regexp = "^[0-9]{5}-[0-9]{3}$", message = "CEP deve estar no formato XXXXX-XXX")
-    private String zipCode;
+    String zipCode,
     @PositiveOrZero(message = "Número não pode ser negativo")
-    private int number;
-    private String complement;
+    int number,
+        String complement,
     @NotEmpty(message = "País não pode ser vazio")
-    private String country;
-
-    public AddressForm() {
-    }
-
-    public AddressForm(String street, String city, String state, String zipCode, String country) {
-        this.street = street;
-        this.city = city;
-        this.state = state;
-        this.country = country;
-    }
-
-    public AddressForm(String street, String city, String state, String zipCode, int number, String complement, String country) {
-        this.street = street;
-        this.city = city;
-        this.state = state;
-        this.zipCode = zipCode;
-        this.number = number;
-        this.complement = complement;
-        this.country = country;
-    }
-
-    public String street() {
-        return street;
-    }
-
-    public String city() {
-        return city;
-    }
-
-    public String state() {
-        return state;
-    }
-
-    public String zipCode() {
-        return zipCode;
-    }
-
-    public int number() {
-        return number;
-    }
-
-    public String complement() {
-        return complement;
-    }
-
-    public String country() {
-        return country;
-    }
+    String country) {
+        
 
 }
--- src/main/java/ufba/br/api/dto/UserForm.java
+++ src/main/java/ufba/br/api/dto/UserForm.java
@@ -3,23 +3,7 @@ package ufba.br.api.dto;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 
-public class UserForm {
-    @NotEmpty(message = "Nome não pode ser vazio")
-    private String name;
-    @NotEmpty(message = "Senha não pode ser nula")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Senha deve conter ao menos 8 caracteres, uma letra maiúscula, uma letra minúscula, um número e um caractere especial")
-    private String password;
-
-    public UserForm(String name, String password) {
-        this.name = name;
-        this.password = password;
-    }
-
-    public String name() {
-        return name;
-    }
-
-    public String password() {
-        return password;
-    }
+public record UserForm(
+        @NotEmpty(message = "Nome não pode ser vazio") String name,
+        @NotEmpty(message = "Senha não pode ser nula") @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Senha deve conter ao menos 8 caracteres, uma letra maiúscula, uma letra minúscula, um número e um caractere especial") String password) {
 }
